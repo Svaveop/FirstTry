64ed5d0d41164b70773f9e4830d86e9657071cee
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-02 21:00:35 +0400

    Updated areg.js

--- areg.js
+++ areg.js
@@ -3,6 +3,9 @@ let alarm = true
 if (alarm = true) {
     console.log("Wake up")
 }
-else{
+elseif (alarm = false){
     console.log("You can still sleep")
+}
+else {
+    console.log("You broke the system?!?!")
 }
